e107cde56952eee6e13a5e5186d0e63d6f82251b
Andre Igor <andreigormenezes@gmail.com>
2022-04-26 15:44:10 -0300

    ok

--- assets/script.js
+++ assets/script.js
@@ -32,18 +32,16 @@ for (let i = 0; i < espacos.length; i++) {
     
 }
 
-console.log(cells)
+// const esp1 = document.getElementById('1')
+// const esp2 = document.getElementById('2')
+// const esp3 = document.getElementById('3')
+// const esp4 = document.getElementById('4')
+// const esp5 = document.getElementById('5')
+// const esp6 = document.getElementById('6')
+// const esp7 = document.getElementById('7')
+// const esp8 = document.getElementById('8')
+// const esp9 = document.getElementById('9')
 
-const esp1 = document.getElementById('1')
-const esp2 = document.getElementById('2')
-const esp3 = document.getElementById('3')
-const esp4 = document.getElementById('4')
-const esp5 = document.getElementById('5')
-const esp6 = document.getElementById('6')
-const esp7 = document.getElementById('7')
-const esp8 = document.getElementById('8')
-const esp9 = document.getElementById('9')
-
-if ((esp1.innerHTML == XSymbol) && (esp2.innerHTML == XSymbol) && (esp3.innerHTML == XSymbol)) {
-    console.log("Jogador X Ganhou!")
-}
+// if ((esp1.innerHTML == XSymbol) && (esp2.innerHTML == XSymbol) && (esp3.innerHTML == XSymbol)) {
+//     console.log("Jogador X Ganhou!")
+// }
